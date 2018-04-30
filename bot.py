c14b3fcb98d9a7f9aa4037dc1b3a699a845906c7
Reverion <34442631+Reverion@users.noreply.github.com>
2018-04-30 20:34:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,5 +28,5 @@ async def on_message(message):
         await client.send_message(message.channel, "%s" % (result))
 
 
-client.run('NDQwNDU3NTMxNDg4NDY5MDEz.DcjupA.ymYfiUoOh35--aI-oIUfZBN9U-E')
+client.run('NDQwNDU3NTMxNDg4NDY5MDEz.Dcjyyw.kDRBU0lXyhn3Th3ywKS-0itIrrs')
 
